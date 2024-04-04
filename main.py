e21cd7b80986174be44b3c22e588d4fb6471a0c5
mrtea9 <ionmeriacri111@gmail.com>
2024-04-04 13:01:50 +0300

    Rain Alert SMS

---
+++ main.py
@@ -0,0 +1,41 @@
+import requests
+from twilio.rest import Client
+
+
+MY_LAT = 47.0056
+MY_LONG = 28.8575
+api_key = ""
+OWM_url = "http://api.openweathermap.org/data/2.5/forecast"
+account_sid = "AC4a180f059febe490c20d7ae861822b0b"
+auth_token = ""
+
+
+parameters = {
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid": api_key,
+    "cnt": 4,
+}
+
+
+response = requests.get(OWM_url, params=parameters)
+response.raise_for_status()
+data = response.json()
+list_data = data["list"]
+
+will_rain = False
+for hour_data in list_data:
+    weather_id = hour_data["weather"][0]["id"]
+    if weather_id < 700:
+        will_rain = True
+        break
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="O sa fie ploaie, vezi cum te imbraci",
+        from_="+12058093489",
+        to="+37369467775",
+    )
+
+    print(message.status)
